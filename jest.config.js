3c6d8ab4ad71d457b5594c531e5cbbfe59dac160
fillmember <fillmember@gmail.com>
2021-12-30 17:44:53 +0100

    setup Jest for monaco theme development

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
+  transformIgnorePatterns: ["/node_modules/(?!monaco-editor).+\\.js$"],
+  moduleNameMapper: {
+    "\\.(css)$": "identity-obj-proxy",
+  },
 };
